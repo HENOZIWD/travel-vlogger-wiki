cae02d45ad48c263c641157579a867ae675e1241
Donghyeon Lee <ehdgus001928@gmail.com>
2026-02-21 18:28:17 +0900

    feat: 콘텐츠 등록 및 편집 시 지도 검색 버튼 표시되지 않도록 수정

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -135,7 +135,7 @@ export const ContentList = () => {
           </InfoWindow>
         )
         : null}
-      {!isLastFetched && !isDragging
+      {!isLastFetched && !isDragging && !(isRegistering || isEditing)
         ? (
           <Button
             type="button"
